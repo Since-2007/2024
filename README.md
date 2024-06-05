39757e5645263132c999b53541327b45d64c1338
2022 <75785505+Since-2007@users.noreply.github.com>
2024-06-05 15:51:24 +0800

    Add foundational files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2024
- 
+
+1. “old_problems”文件夹存储是原始题目。
+
+2. “new_problems_adapted”文件夹存储通过单个题目改编得到的新题目。
+
+3. “new_problems_assembly”文件夹存储通过多个题目重组得到的新题目。
+
+4. 在浏览器中打开相应文件夹下面的“index.html”文件即可查看题目详细信息。
---
+++ old_problems/data/data.js
@@ -0,0 +1,5 @@
+var problems = [
+{ptxt:"有三角形ABC,有一点D,且有D为AB的中点,有一点E,且有B,C,E共线,且有BE:EC=2:1。若2CD=3AE,求证:AB⊥AC ",pimg:"images/t_1.png",ans:""},
+{ptxt:"有三角形ABC,有一点D,且有C为DA的中点,有一点E,且有A为EB的中点。若BD=2CE,求证:AB⊥AC ",pimg:"images/t_2.png",ans:""},
+{ptxt:"有三角形ABC,有一点D,且有C为DB的中点,有一点E,且有E,C,A共线,且有EA:AC=2:1。若AB⊥AC,求证:AD=BE ",pimg:"images/t_3.png",ans:""}];
+	
